bc7529af70268808e60c7c8d6378d8f149e8beb5
Yasser SAADI <yassirsaadi4@gmail.com>
2025-05-31 15:07:13 +0100

    Adding unit tests for handleSeasonalProduct

--- api/src/test/java/com/nimbleways/springboilerplate/services/implementations/ProductServiceUnitTest.java
+++ api/src/test/java/com/nimbleways/springboilerplate/services/implementations/ProductServiceUnitTest.java
@@ -105,4 +105,65 @@ class ProductServiceUnitTest {
         Mockito.verify(productRepository, Mockito.times(1)).save(product);
         Mockito.verify(notificationService, Mockito.times(1)).sendExpirationNotification(product.getName(), product.getExpiryDate());
     }
+
+
+    @Test
+    void handle_seasonal_product_should_send_out_of_stock_notification_when_product_will_not_be_available_before_the_end_of_the_season() {
+        Product product = new Product(null,
+                10,
+                10,
+                "NORMAL",
+                "RJ45 Cable",
+                null,
+                null,
+                LocalDate.now().plusDays(9));
+
+
+        Mockito.when(productRepository.save(product)).thenReturn(product);
+
+        productService.handleSeasonalProduct(product);
+        assertThat(product.getAvailable()).isZero();
+        Mockito.verify(productRepository, Mockito.times(1)).save(product);
+        Mockito.verify(notificationService, Mockito.times(1)).sendOutOfStockNotification(product.getName());
+    }
+
+    @Test
+    void handle_seasonal_product_should_send_out_of_stock_notification_when_product_season_start_date_is_after_today_date() {
+        Product product = new Product(null,
+                10,
+                10,
+                "NORMAL",
+                "RJ45 Cable",
+                null,
+                LocalDate.now().plusDays(1),
+                LocalDate.now().plusDays(10));
+
+
+        Mockito.when(productRepository.save(product)).thenReturn(product);
+
+        productService.handleSeasonalProduct(product);
+        assertThat(product.getAvailable()).isEqualTo(10);
+        Mockito.verify(productRepository, Mockito.times(1)).save(product);
+        Mockito.verify(notificationService, Mockito.times(1)).sendOutOfStockNotification(product.getName());
+    }
+
+    @Test
+    void handle_seasonal_product_should_send_delay_notification_when_seasonal_product_is_available() {
+        Product product = new Product(null,
+                10,
+                10,
+                "NORMAL",
+                "RJ45 Cable",
+                null,
+                LocalDate.now().minusDays(1),
+                LocalDate.now().plusDays(10));
+
+
+        Mockito.when(productRepository.save(product)).thenReturn(product);
+
+        productService.handleSeasonalProduct(product);
+        assertThat(product.getAvailable()).isEqualTo(10);
+        Mockito.verify(productRepository, Mockito.times(1)).save(product);
+        Mockito.verify(notificationService, Mockito.times(1)).sendDelayNotification(product.getLeadTime(), product.getName());
+    }
 }
